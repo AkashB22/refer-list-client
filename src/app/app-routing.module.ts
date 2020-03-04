1436c9f94f9121fb84fccf6b3e0d6744b647db0b
akash <akashbalu22@gmail.com>
2020-03-04 11:23:44 +0530

    adding bootstrap for styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import {SignUpComponent} from './sign-up/sign-up.component';
 import {ProfileComponent} from './profile/profile.component';
 
 const routes: Routes = [
+  {path: '', redirectTo: 'signup', pathMatch: 'full'},
   {path: 'signup', component: SignUpComponent},
   {path: 'signup/:uniqueId', component: SignUpComponent},
   {path: 'signin', component: SigninComponent},
